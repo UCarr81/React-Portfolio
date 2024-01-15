5f753c7a48ecac284e7a9d3f23a2177d169eaa45
Uriel <CarrilloUriel81@gmail.com>
2024-01-15 13:51:06 -0800

    Updated Work file

--- portfolio-react-app/src/components/Work.jsx
+++ portfolio-react-app/src/components/Work.jsx
@@ -1,40 +1,57 @@
-import React from 'react'
+import React from 'react';
 import SkinScoreImage from '../assets/SkinScore.png';
 import FilmPulse from '../assets/FilmPulse.png';
+import Weather from '../assets/Weather.png';
+import ETracker from '../assets/EmployeeTracker.png';
+import './Work.css';
 
 const Work = () => {
+  const projects = [
+    {
+      image: SkinScoreImage,
+      demoLink: '/',
+      title: 'SkinScore',
+    },
+    {
+      image: FilmPulse,
+      demoLink: '/',
+      title: 'FilmPulse',
+    },
+    {
+      image: Weather,
+      demoLink: '/',
+      title: 'Weather',
+    },
+    {
+      image: ETracker,
+      demoLink: '/',
+      title: 'Emp. Tracker',
+    },
+  ];
+
   return (
-    <div name='work' className='w-full md:h-screen bg-gray-700 text-white'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-yellow-300 text-white'>Work</p>
-                <p className='py-6'>Check out my Recent projects/work</p>
-            </div>
-
-            <div style={{backgroundImage: `url(${SkinScoreImage})`, height: '250px', backgroundSize: 'cover'}} 
-            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-gray-400 group container rounded-md flex justify-Center items-center mx-auto content-div'>
-                    
-
-                <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'> SkinScore
-
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-yellow-200 text-red-400 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-yellow-200 text-red-400 font-bold text-lg'>Code</button>
-                        </a>
-                        
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div className="w-full mx-auto p-8 bg-gray-800 md:-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {projects.map((project, index) => (
+          <div key={index} className="mb-4 relative group overflow-hidden transition-all duration-300 hover:bg-transparent">
+            <div className="image-overlay"></div>
+            <img src={project.image} alt={project.title} className="w-full h-[200px] transition-transform duration-300 transform group-hover:scale-105" />
+            <a
+              href={project.demoLink}
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 group-hover:scale-110"
+            >
+              <span className="text-2xl font-bold text-white items-center flex" style={{ WebkitTextStroke: '1px black' }}>
+                {project.title}
+              </span>
+              <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-black font-bold text-lg'>
+                Demo
+              </button>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
